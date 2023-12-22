59a27db3023a9561778d8ef65e9e28eefdd2cf1e
ckim48 <ckim48@u.rochester.edu>
2023-12-22 11:18:53 +0900

    sss

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -257,12 +257,7 @@
    * Animation on scroll
    */
   window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
+    AOS.init();
   });
 
   /**
